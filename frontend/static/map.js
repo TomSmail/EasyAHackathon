ce66d800c76c776f5cbd650473cfadf73c44f643
TomSmail <thomas.c.smail@gmail.com>
2023-06-24 19:10:20 +0100

    Feat: Implements arcs between nodes

--- frontend/static/map.js
+++ frontend/static/map.js
@@ -9,24 +9,35 @@ L.tileLayer('https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token
 	accessToken: 'PJ7QkPxNaAADH2vKKJQO4J9zYicPbQjWt7PkGSQs9WWxMr4ea3y7uRmxoJoDsXKV',
 }).addTo(map);
 
-var flag  = L.icon({
+var pin  = L.icon({
     iconUrl: 'https://www.pngall.com/wp-content/uploads/2/Drawing-Pin.png',
     iconSize:     [20, 20],
-    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [10, 10] // point from which the popup should open relative to the iconAnchor
+    iconAnchor:   [10, 10],
+    popupAnchor:  [0, 0]
 });
 
 // Plots nodes onto map for each step in diamonds history
 function plotPoint(lat, lon, address, owner_rep, certification) {
-    var marker = L.marker([lat, lon], {icon: flag}).addTo(map);
+    var marker = L.marker([lat, lon], {icon: pin}).addTo(map);
     marker.bindPopup(`<p> Owner Address: ${address} <br/> Owner Reputation: ${owner_rep} <br/> Certificate: ${certification} </p>`);
+    return marker;
 }
 
-// address, owner reputability, 
+function drawArc(marker1, marker2) {
+    L.Polyline.Arc(marker1.getLatLng(), marker2.getLatLng(), {
+        color: 'red',
+        vertices: 500
+    }).addTo(map);
+}
 
-plotPoint(35.126413,33.429859, "Cypress", "4 Stars", "Cert");
-plotPoint(50.503887,4.469936, "Belgium", "5 Stars", "Cert");
-plotPoint(46.227638,2.213749, "France", "5 Stars", "Cert");
-plotPoint(55.378051,-3.435973, "UK", "4 Stars", "Cert");
-plotPoint(31.791702,-7.09262, "Morocco", "4 Stars", "Cert");
+// Example : 
+var p1 = plotPoint(35.126413,33.429859, "Cypress", "4 Stars", "Cert");
+var p2 = plotPoint(50.503887,4.469936, "Belgium", "5 Stars", "Cert");
+var p3 = plotPoint(46.227638,2.213749, "France", "5 Stars", "Cert");
+var p4 = plotPoint(55.378051,-3.435973, "UK", "4 Stars", "Cert");
+var p5 = plotPoint(31.791702,-7.09262, "Morocco", "4 Stars", "Cert");
+
+drawArc(p1, p2);
+drawArc(p2, p3);
+drawArc(p3, p4);
+drawArc(p4, p5);
